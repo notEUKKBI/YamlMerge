54dd4582d84b02e161bf860e59a9574d46199fa5
notEUKKBI <smbaekr68b@gmail.com>
2024-07-31 16:07:16 +0900

    asd
    asd

---
+++ Assets/add.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class add : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ÇÑ¿ì¾Û ¿ì¾Û
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
