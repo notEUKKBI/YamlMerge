d25817e57c11bbe5ed1a9ba5fa5991ffceeec136
notEUKKBI <smbaekr68b@gmail.com>
2024-10-14 18:14:38 +0900

    asda
    sdasd

---
+++ Assets/add.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class add : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> m;
+
+    [SerializeField]
+    private List<string> m3;
+
+    private List<string> m2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
